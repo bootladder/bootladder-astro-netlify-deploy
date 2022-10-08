61a2cacf96236138777e9b0eb88ccbcedc42cccf
Steve Anderson <steve@bootladder.com>
2022-10-08 09:09:20 -0400

    permalinks move to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,43 @@ import sitemap from '@astrojs/sitemap';
 import image from '@astrojs/image';
 import partytown from '@astrojs/partytown';
 
-import { SITE } from './src/config.mjs';
+export const SITE = {
+	name: 'AstroWind',
+
+	origin: 'https://bootladder.com/',
+	basePathname: '/',
+
+	title: 'Bootladder Engineering',
+	description: 'Firmware for the 21st Century',
+
+	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
+	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
+};
+
+export const BLOG = {
+	disabled: false,
+	postsPerPage: 4,
+
+	blog: {
+		disabled: false,
+		pathname: 'blog', // blog main path, you can change this to "articles" (/articles)
+	},
+
+	post: {
+		disabled: false,
+		pathname: '', // empty for /some-post, value for /pathname/some-post 
+	},
+
+	category: {
+		disabled: false,
+		pathname: 'category', // set empty to change from /category/some-category to /some-category
+	},
+
+	tag: {
+		disabled: false,
+		pathname: 'tag', // set empty to change from /tag/some-tag to /some-tag
+	},
+};
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
